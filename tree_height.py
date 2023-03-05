39dad822994f2733e2f42b279d60700d2c969be3
MartinsViksna <125703411+MartinsViksna@users.noreply.github.com>
2023-03-05 02:11:19 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -36,7 +36,7 @@ def main():
 
             with open(file_name)as file:
                 n = int(file.readline())
-                parents = np.frombuffer(file.readline().strip(), dtype=int)
+                parents = np.array( list(map(int, f.readline().split())))
         except IOError:
             print("no such file")
             return
